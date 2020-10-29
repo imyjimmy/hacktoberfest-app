6d62e1cd9156de1e0bbd97b070643d4f9c9aad94
marcelorl <marcelo_rl2@hotmail.com>
2020-10-29 03:02:37 -0300

    fix: bootstrap integrity number was incorrect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, Switch } from "react-router-dom";
+import { Route, Switch } from 'react-router-dom';
 import Header from './navbar/Header';
 import Home from './Home';
 import About from './About';
@@ -8,20 +8,18 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Switch>
-          <Route exact path="/">
-            <Home/>
-          </Route>
-          <Route path="/about">
-            <About>Hacktoberfest happens on October, and what happens on October is Hacktobefest. This fest is hosted every year of October.</About>
-          </Route>
-        </Switch>
-        <Footer/>
-      </div>
-    </Router>
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/about">
+          <About>Hacktoberfest happens on October, and what happens on October is Hacktobefest. This fest is hosted every year of October.</About>
+        </Route>
+      </Switch>
+      <Footer/>
+    </div>
   );
 }
 
